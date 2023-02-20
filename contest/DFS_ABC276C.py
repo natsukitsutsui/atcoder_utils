d46c80223c45e4c6101b18715d48b9850b9e1aa3
Natsuki Tsutsui <natsuki.tsutsui.08@gmail.com>
2023-02-20 20:58:56 +0900

    fix

--- contest/DFS_ABC276C.py
+++ contest/DFS_ABC276C.py
@@ -5,22 +5,23 @@ https://atcoder.jp/contests/abc276/tasks/abc276_e
 """
 import sys
 sys.setrecursionlimit(10**7)
- 
-H, W = map(int,input().split())
+
+H, W = map(int, input().split())
 C = []
 for _ in range(H):
     row = list(input())
     C.append(row)
- 
+
 for h in range(H):
     for w in range(W):
         if C[h][w] == "S":
             sx, sy = h, w
- 
+
+
 def dfs(x, y, d):
     if y >= W or y < 0 or x >= H or x < 0 or C[x][y] == "#":
         return
-    
+
     if d == 0 and C[x][y] == "S":
         pass
     elif d < 4 and C[x][y] == "S":
@@ -30,11 +31,12 @@ def dfs(x, y, d):
         exit()
     else:
         C[x][y] = "#"
- 
+
     dfs(x+1, y, d+1)
     dfs(x-1, y, d+1)
     dfs(x, y+1, d+1)
     dfs(x, y-1, d+1)
- 
+
+
 dfs(sx, sy, 0)
 print('No')
