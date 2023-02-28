b10c92d68cadf304f28f1a4de1451aa917374ec7
Natsuki Tsutsui <natsuki.tsutsui.08@gmail.com>
2023-02-28 19:09:08 +0900

    fix: input.py

--- input.py
+++ input.py
@@ -1,2 +1,12 @@
-def LI(): return list(map(int, input().split()))
+from collections import Counter, defaultdict, deque
+from heapq import heappop, heappush, heapify
+from bisect import bisect_left, bisect_right
+import sys, math, itertools, pprint, fractions
+sys.setrecursionlimit(10**8)
+mod1 = 998244353
+mod2 = 10**9+7
+INF = 10**100
+def LMI(): return list(map(int, input().split()))
 def II(): return int(input())
+def LI(): return list(input())
+def IN(): return input()
