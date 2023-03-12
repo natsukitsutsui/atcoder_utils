0df9a8ab1740db9d450c07e6707cd61e22e3284e
nattonato <natsuki.tsutsui.08@gmail.com>
2023-03-12 20:32:56 +0900

    add: geometry

--- contest/DP_kitaiti_ABC280E.py
+++ contest/DP_kitaiti_ABC280E.py
@@ -11,19 +11,47 @@ def II(): return int(input())
 def LI(): return list(input())
 def IN(): return input()
 
-N, P = LMI()
 
-def Division(a: int, b: int, m: int):
-    return (a * pow(b, m-2, m)) % m
+H, W = LMI()
+G = [LI() for _ in range(H)]
+visited = [[False]*W for _ in range(H)]
+visited[0][0] = True
 
-p = Division(P, 100, mod1)
-p_ = Division(100-P, 100, mod1)
+sx, sy = 0, 0
+while True:
+    if G[sx][sy] == 'from collections import Counter, defaultdict, deque
+from heapq import heappop, heappush, heapify
+from bisect import bisect_left, bisect_right
+import sys, math, itertools, pprint, fractions
+sys.setrecursionlimit(10**8)
+mod1 = 998244353
+mod2 = 10**9+7
+INF = 10**100
+def LMI(): return list(map(int, input().split()))
+def II(): return int(input())
+def LI(): return list(input())
+def IN(): return input()
 
-dp = [0]*(N+1)
 
-dp[1] = 1
+N, M, T = LMI()
+A = [0] + LMI()
+X = []
+Y = []
+for _ in range(M):
+    x,y = LMI()
+    X.append(x)
+    Y.append(y)
 
-for i in range(2, N+1):
-    dp[i] = ((dp[i-2]+1)*p + (dp[i-1]+1)*p_) % mod1
+x_i = 0
+for i in range(1, N):
+    if x_i < M and i == X[x_i]:
+        T += Y[x_i]
+        x_i += 1
+    
+    if T > A[i]:
+        T -= A[i]
+    else:
+        print("No")
+        exit()
 
-print(dp[N])
+print("Yes")'
