f6742d070613d371e07192b3447b2362684f0065
Natsuki Tsutsui <natsuki.tsutsui.08@gmail.com>
2023-04-07 15:55:30 +0900

    fix

--- input.py
+++ input.py
@@ -2,7 +2,6 @@ from collections import Counter, defaultdict, deque
 from heapq import heappop, heappush, heapify
 from bisect import bisect_left, bisect_right
 import sys, math, itertools, pprint, fractions
-import numpy as np
 sys.setrecursionlimit(10**8)
 mod1 = 998244353
 mod2 = 10**9+7
