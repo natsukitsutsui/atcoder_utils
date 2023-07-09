2e7c3e2f27fa862b4a6aa93823979f72f4267dff
Natsuki <106019457+natsukitsutsui@users.noreply.github.com>
2023-07-09 22:47:49 +0900

    Update input.py

--- input.py
+++ input.py
@@ -5,7 +5,7 @@ import sys, math, itertools, pprint, fractions
 sys.setrecursionlimit(10**8)
 mod1 = 998244353
 mod2 = 10**9+7
-INF = 10**100
+INF = math.inf
 def LMI(): return list(map(int, input().split()))
 def II(): return int(input())
 def LI(): return list(input())
